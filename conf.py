fcd7a8c1361ec46a4d819da33ca0ce0815f166e6
Varada MV <36720961+shinevarada@users.noreply.github.com>
2019-06-08 12:20:22 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,6 @@
+"""Configurations for telegram_alert.py"""
+bolt_api_key = "XXXX"                    #  Cloud API Key
+device_id = "XXXX"                  # device ID 
+telegram_chat_id = "@XXXX"          # This is the channel ID of the created Telegram channel. 
+telegram_bot_id = "botXXXX"         # This is the bot ID of the created Telegram Bot.
+threshold = 250                     # Threshold beyond which the alert should be sent
